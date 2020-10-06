8ce42ffac0d46f82e3f1fa614bfdcaa8f87c9139
海老名翔平 <ebina.shohei.0613@gmail.com>
2020-10-06 21:01:39 +0900

    user model no

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :categories, dependent: :nullfy
-  has_many :services, dependent: :nullfy
+  has_many :categories, dependent: :destroy
+  has_many :services, dependent: :destroy
   has_many :customers, dependent: :destroy
 
   devise :database_authenticatable, :registerable,
