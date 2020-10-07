8663a958347440690f34c74ee452e9bd59ccc0d3
海老名翔平 <ebina.shohei.0613@gmail.com>
2020-10-07 13:37:22 +0900

    git pull origin master

--- config/routes.rb
+++ config/routes.rb
@@ -8,3 +8,4 @@ Rails.application.routes.draw do
   # get '/users/sign_out' => 'devise/sessions#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
